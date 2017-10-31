4cffe15dc60f14039f9fe2d98c9a969a06a6bd40
princessguerrero <princessguerrero@ac.c4q.nyc>
2017-10-31 13:52:25 -0400

    finished the exercise

---
+++ DOM-2/re-coloredQuads/recolored-quadrants.js
@@ -0,0 +1,4 @@
+var identifier = window.prompt("Enter an identifier.");
+var color = window.prompt("Enter a color");
+var input = document.querySelector(identifier);
+input.style.backgroundColor = color
